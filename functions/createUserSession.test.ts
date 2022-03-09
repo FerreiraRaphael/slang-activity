2c89dd0d2d6eb6bc66b5cc823d11022dd68ac46d
Raphael <raphaelbsferreira@gmail.com>
2022-03-09 13:36:07 -0300

    createUserSession first tests pass

--- functions/createUserSession.test.ts
+++ functions/createUserSession.test.ts
@@ -1,4 +1,4 @@
-import { Activity, GroupedUserSession, createUserSession } from './createUserSession';
+import { Activity, GroupedUserSession, createGroupedUserSessions } from './createUserSession';
 
 describe('createUserSession function', () => {
   const activities: Activity[] = [
@@ -31,22 +31,22 @@ describe('createUserSession function', () => {
     const result: GroupedUserSession = {
       "1nqfprq4": [{
         activity_ids: [96272, 254232],
-        duration_seconds: 0,
+        duration_seconds: 308,
         started_at: "2022-02-12T02:53:58.598+00:00",
         ended_at: "2022-02-12T02:59:06.598+00:00",
       }, {
         activity_ids: [102107],
-        duration_seconds: 0,
+        duration_seconds: 13,
         started_at: "2022-02-12T03:04:27.598+00:00",
         ended_at: "2022-02-12T03:04:40.598+00:00",
       }],
       "967o94ff": [{
-        activity_ids: [102107],
-        duration_seconds: 0,
-        started_at: "2022-02-12T03:04:27.598+00:00",
-        ended_at: "2022-02-12T03:04:40.598+00:00",
+        activity_ids: [84322],
+        duration_seconds: 16,
+        started_at: "2022-02-12T00:05:26.598+00:00",
+        ended_at: "2022-02-12T00:05:42.598+00:00"
       }]
     }
-    expect(createUserSession(activities)).toBe(result);
+    expect(createGroupedUserSessions(activities)).toStrictEqual(result);
   })
 })
--- functions/createUserSession.ts
+++ functions/createUserSession.ts
@@ -1,3 +1,5 @@
+import { getDateDiffSeconds } from "./getDateDiff";
+
 export interface Activity {
   id: number;
   user_id: string;
@@ -13,10 +15,54 @@ export interface UserSession {
 }
 
 export interface GroupedUserSession {
-  [userId: string]: Array<UserSession>
+  [userId: string]: Array<UserSession>;
 }
 
+const FIVE_MINUTES = 60 * 5 // In seconds.
 
-export function createUserSession(activities: Activity[]): GroupedUserSession {
-  return {};
+function createUserSession(activity: Activity): UserSession {
+  const startDate = new Date(activity.first_seen_at);
+  const endDate = new Date(activity.answered_at);
+  return {
+    activity_ids: [activity.id],
+    ended_at: activity.answered_at,
+    started_at: activity.first_seen_at,
+    duration_seconds: getDateDiffSeconds(startDate, endDate),
+  };
+}
+
+function addActivityToUserSession(userSession: UserSession, activity: Activity): UserSession {
+  const startDate = new Date(userSession.started_at);
+  const endDate = new Date(activity.answered_at);
+  return {
+    ...userSession,
+    activity_ids: [...userSession.activity_ids, activity.id],
+    ended_at: activity.answered_at,
+    duration_seconds: getDateDiffSeconds(startDate, endDate),
+  }
+}
+
+export function createGroupedUserSessions(activities: Activity[]): GroupedUserSession {
+  const groupedUserSessions: GroupedUserSession = {};
+  for (let index = 0; index < activities.length; index++) {
+    const activity = activities[index];
+    const userSessions = groupedUserSessions[activity.user_id];
+    if (userSessions) {
+      const lastUserSession = userSessions[userSessions.length - 1];
+      const sessionEndedAt = new Date(lastUserSession.ended_at);
+      const activityStartedAt = new Date(activity.first_seen_at);
+      const isMoreThenFiveMin = getDateDiffSeconds(sessionEndedAt, activityStartedAt) > FIVE_MINUTES;
+      if (isMoreThenFiveMin) {
+        // add new session
+        userSessions.push(createUserSession(activity));
+      } else {
+        // add to session
+        userSessions[userSessions.length - 1] = addActivityToUserSession(lastUserSession, activity);
+      }
+    } else {
+      // create new user session
+      groupedUserSessions[activity.user_id] = [createUserSession(activity)];
+    }
+  }
+  return groupedUserSessions;
 }
